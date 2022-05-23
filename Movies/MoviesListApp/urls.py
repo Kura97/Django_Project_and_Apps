acb8c029912b4c64ffded801ed0e61334de24465
Abdulaziz Altariqi <AbdulazizAlturaiqi@gmail.com>
2022-05-23 13:33:48 +0300

    LAB15 done!

--- Movies/MoviesListApp/urls.py
+++
@@ -1,9 +0,0 @@
-
-import MoviesListApp.views
-from django.urls import path
-
-urlpatterns = {
-
-    path('', MoviesListApp.views.index),
-    path('SearchResult/', MoviesListApp.views.SearchResult)
-}
